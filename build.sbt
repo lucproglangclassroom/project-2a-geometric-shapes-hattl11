c34a27d6f12df707b9b9e521c33e4acbd6df25ff
gravati <160966269+hattl11@users.noreply.github.com>
2025-10-20 20:04:25 -0500

    Added logging

--- build.sbt
+++ build.sbt
@@ -5,5 +5,6 @@ version := "0.4"
 libraryDependencies ++= Seq(
   "org.creativescala" %% "doodle"     % "0.30.0",
   "org.scalatest"     %% "scalatest"  % "3.2.19"  % Test,
-  "org.scalacheck"    %% "scalacheck" % "1.18.1"  % Test
+  "org.scalacheck"    %% "scalacheck" % "1.18.1"  % Test,
+  "org.slf4j"          % "slf4j-simple" % "2.0.12"
 )
--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -1,33 +1,41 @@
 package edu.luc.cs.laufer.cs371.shapes
 
-// TODO: implement this behavior
-
 import Shape.*
+import org.slf4j.LoggerFactory
 
 object boundingBox:
-  def apply(s: Shape): Location = s match
-    case Rectangle(w, h) =>
-      Location(0, 0, Rectangle(w, h))
+  // simple named logger to avoid getClass quirks
+  private val log = LoggerFactory.getLogger("boundingBox")
 
-    case Ellipse(rx, ry) =>
-      Location(0, 0, Rectangle(2 * rx, 2 * ry)) // diameters
+  def apply(s: Shape): Location =
+    s match
+      case Rectangle(w, h) =>
+        log.debug(s"Computing bbox for Rectangle($w, $h)")
+        Location(0, 0, Rectangle(w, h))  // <-- last expr returns Location
 
-    case Location(x, y, inner) =>
-      val Location(x0, y0, Rectangle(w, h)) = apply(inner)
-      Location(x + x0, y + y0, Rectangle(w, h))
+      case Ellipse(rx, ry) =>
+        log.debug(s"Computing bbox for Ellipse($rx, $ry)")
+        Location(0, 0, Rectangle(2 * rx, 2 * ry))
 
-    case Group(children*) =>
-      val boxes = children.map(apply) // Seq[Location]
-      // left/top/right/bottom per child
-      val lefts   = boxes.map(_.x)
-      val tops    = boxes.map(_.y)
-      val rights  = boxes.map(b => b.x + b.shape.asInstanceOf[Rectangle].width)
-      val bottoms = boxes.map(b => b.y + b.shape.asInstanceOf[Rectangle].height)
+      case Location(x, y, inner) =>
+        val Location(x0, y0, rect) = apply(inner)
+        log.debug(s"Shifting bbox by ($x, $y)")
+        Location(x + x0, y + y0, rect)
 
-      val minX = lefts.min
-      val minY = tops.min
-      val maxX = rights.max
-      val maxY = bottoms.max
+      case Group(children*) =>
+        log.info(s"Computing bbox for Group of ${children.size} shapes")
+        val boxes   = children.map(apply) // Seq[Location]
+        val lefts   = boxes.map(_.x)
+        val tops    = boxes.map(_.y)
+        val rights  = boxes.map(b => b.x + b.shape.asInstanceOf[Rectangle].width)
+        val bottoms = boxes.map(b => b.y + b.shape.asInstanceOf[Rectangle].height)
 
-      Location(minX, minY, Rectangle(maxX - minX, maxY - minY))
+        val minX = lefts.min
+        val minY = tops.min
+        val maxX = rights.max
+        val maxY = bottoms.max
+
+        val rect = Rectangle(maxX - minX, maxY - minY)
+        log.debug(s"Result bbox: Location($minX, $minY, $rect)")
+        Location(minX, minY, rect)        // <-- last expr returns Location
 end boundingBox
