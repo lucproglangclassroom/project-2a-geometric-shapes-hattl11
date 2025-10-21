f16bcab77a66405912175fdd34c6fc76fbf107e2
gravati <160966269+hattl11@users.noreply.github.com>
2025-10-20 19:23:59 -0500

    init BB and shapes fixtures/test

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -6,6 +6,28 @@ import Shape.*
 
 object boundingBox:
   def apply(s: Shape): Location = s match
-    case _ => Location(0, 0, Rectangle(0, 0)) // not yet implemented
+    case Rectangle(w, h) =>
+      Location(0, 0, Rectangle(w, h))
 
+    case Ellipse(rx, ry) =>
+      Location(0, 0, Rectangle(2 * rx, 2 * ry)) // diameters
+
+    case Location(x, y, inner) =>
+      val Location(x0, y0, Rectangle(w, h)) = apply(inner)
+      Location(x + x0, y + y0, Rectangle(w, h))
+
+    case Group(children*) =>
+      val boxes = children.map(apply) // Seq[Location]
+      // left/top/right/bottom per child
+      val lefts   = boxes.map(_.x)
+      val tops    = boxes.map(_.y)
+      val rights  = boxes.map(b => b.x + b.shape.asInstanceOf[Rectangle].width)
+      val bottoms = boxes.map(b => b.y + b.shape.asInstanceOf[Rectangle].height)
+
+      val minX = lefts.min
+      val minY = tops.min
+      val maxX = rights.max
+      val maxY = bottoms.max
+
+      Location(minX, minY, Rectangle(maxX - minX, maxY - minY))
 end boundingBox
